92932fb9b8d4d170d0a6e9e01789243860a66b45
student <kotyadasushmithad@gmail.com>
2019-02-25 18:14:43 -0600

    Update Settings Page

---
+++ Lunar Lander/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  Lunar Lander
+//
+//  Created by student on 2/25/19.
+//  Copyright © 2019 Northwest. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
