ac8e1b4050b854d801e43bb8a38fa72259578e93
rayaan ahmed <rayaan@rayaans-macbook-air.local>
2019-02-25 18:57:36 -0600

    Update Home

--- Lunar Lander/FirstViewController.swift
+++ Lunar Lander/FirstViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    
+    @IBAction func Play_Button(_ sender: Any) {
+        print("Clicked")
+        //need to redirict to another storyboard
+        
+    }//end of method
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }//end of viewDidLoad method
 
 
-}
+}//end of class
 
